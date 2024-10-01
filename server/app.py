b812d88a1fc18c2a8448acb3aaeb47ecab578525
root <root@DESKTOP-RHA5CJR.localdomain>
2024-10-02 02:26:28 +0300

    Flask Routing and Views Lab

--- server/app.py
+++ server/app.py
@@ -4,5 +4,38 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(parameter)
+    return f'{parameter}'
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    numbers = '<br>'.join(str(i)for i in range(parameter))
+    return numbers
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, operation, num2):
+    
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':  # Division
+        result = num1 / num2
+    elif operation == '%':  # Modulus
+        result = num1 % num2
+    else:
+        return 'Invalid operation! Use +, -, *, div, or %.'
+
+    return f' {num1} {operation} {num2} = {result}'
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
